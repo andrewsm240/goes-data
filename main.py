afa939df647fbaf4d8dcb8ce9684b6877ed497fd
andrewsm240 <76793520+andrewsm240@users.noreply.github.com>
2021-07-22 13:09:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
 #!/usr/bin/env python3
-import argparse
-import logging
-import time
 import numpy as np
-from waggle import plugin
-from waggle.data.vision import Camera
+import s3fs
+import datetime
+import os
 
 
-def process_frame(frame):
-    # we assume frame shape is (H, W, 3) for an RGB image
-    mean = np.mean(frame, (0, 1))
-    min = np.min(frame, (0, 1))
-    max = np.min(frame, (0, 1))
-    return {
-        "mean": mean,
-        "min": min,
-        "max": max,
-    }
+# Find Julian day from given year/month/day
+def julian(year_name, month_name, day_name):
+    
+    calendar = datetime.datetime(year_name, month_name, day_name)
+    julian_day = calendar.strftime('%j')
+    
+    return julian_day
+
+
+# Create list of ABI data file names for given satellite, product, and datetime period
+def aws_list(year_name, month_name, day_name, starthour, startmin, endhour, endmin, satellite_name, view_name, sat_band, product):
+    
+    # Access AWS using anonymous credentials
+    aws = s3fs.S3FileSystem(anon=True)
+    
+    # Make a list of all data files for given date and start/end hours
+    julian_day = julian(year_name, month_name, day_name)
+    hour_range = range(int(starthour), int(endhour) + 1)
+    all_hours_list = []
+    
+    for i in hour_range:
+        hour_files = aws.ls('noaa-goes' + str(satellite_name) + '/' + product + '/' + str(year_name) + '/' + julian_day + '/' + str(i) + '/', refresh=True)
+        all_hours_list.extend(hour_files)
+    
+    # Extract list of data files for specified period set by start/end times
+    data = []
+    
+    # List file names
+    for i in all_hours_list:
+        if view_name == 'CONUS': 
+
+            # Select the files that are between the desired start and end time
+            if i[-42:-38] >= (starthour + startmin) and i[-26:-22] <= (endhour + endmin):  
+
+                # Account for the change in default scan mode of the ABI
+                if i[-60:-57] == 'M3C' or i[-60:-57] == 'M6C':
+                    
+                    # Get desired band/channel
+                    if i[-57:-55]  == sat_band:
+                        data.append(i)
+        else:
+            continue
+
+    return data
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--device", default=0, help="camera device to use")
-    parser.add_argument("--rate", default=10, type=float, help="sampling interval in seconds")
-    args = parser.parse_args()
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(message)s',
-        datefmt='%Y/%m/%d %H:%M:%S')
-
-    logging.info("starting plugin. will process a frame every %ss", args.rate)
 
     plugin.init()
-
-    cam = Camera(args.device)
-
-    for sample in cam.stream():
-        results = process_frame(sample.data)
-
-        logging.info("results %s", results)
-
-        plugin.publish("image.mean.red", results["mean"][0])
-        plugin.publish("image.mean.green", results["mean"][1])
-        plugin.publish("image.mean.blue", results["mean"][2])
-
-        plugin.publish("image.min.red", results["min"][0])
-        plugin.publish("image.min.green", results["min"][1])
-        plugin.publish("image.min.blue", results["min"][2])
-
-        plugin.publish("image.max.red", results["max"][0])
-        plugin.publish("image.max.green", results["max"][1])
-        plugin.publish("image.max.blue", results["max"][2])
-
-        time.sleep(args.rate)
 
 
 if __name__ == "__main__":
